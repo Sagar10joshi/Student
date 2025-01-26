004b68f4777be216476cfad4dccb791524812e8a
Sagar Joshi <coderjoshi15@gmail.com>
2025-01-26 19:02:37 +0530

    last1 commit

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -5,3 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+
+// // Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAqpksr5sLf1lIIMlYoSibBB6MBVQLLOK8",
+//   authDomain: "student-34336.firebaseapp.com",
+//   projectId: "student-34336",
+//   storageBucket: "student-34336.firebasestorage.app",
+//   messagingSenderId: "400850052517",
+//   appId: "1:400850052517:web:8eeeb847648b66eeeab44c",
+//   measurementId: "G-GXXWG3287Y",
+// };
